922ebb5de0f81a57b58b9d13dece938901ab0c90
diptanu <diptanuc@gmail.com>
2024-08-24 01:39:29 +0000

    using pdf parser

--- pdf/pdfextractor/pdf_extractor.py
+++ pdf/pdfextractor/pdf_extractor.py
@@ -1,20 +1,12 @@
-import json
-import tempfile
-from typing import List, Literal, Optional, Union
+from typing import List, Optional, Union
 
-import fitz
-import pymupdf
-import pymupdf4llm
-import os
 from indexify.extractor_sdk import Content, Extractor, Feature
+from indexify.extractors.pdf_parser import Page, PageFragmentType, PDFParser
 from pydantic import BaseModel, Field
-
-from .utils.tt_module import get_tables
 
 
 class PDFExtractorConfig(BaseModel):
-    output_types: List[str] = Field(default_factory=lambda: ["text"])
-    output_format: Literal["markdown", "text"] = "markdown"
+    language: Optional[str] = "en"
 
 
 class PDFExtractor(Extractor):
@@ -30,58 +22,23 @@ class PDFExtractor(Extractor):
         self, content: Content, params: PDFExtractorConfig
     ) -> List[Union[Feature, Content]]:
         contents = []
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as inputtmpfile:
-            inputtmpfile.write(content.data)
-            inputtmpfile.flush()
-
-            if "text" in params.output_types:
-                if params.output_format == "markdown":
-                    md_text = pymupdf4llm.to_markdown(inputtmpfile.name)
-                    contents.append(Content.from_text(md_text))
-                else:
-                    with pymupdf.open(inputtmpfile.name) as doc:
-                        for page_num, page in enumerate(doc):
-                            text = page.get_text()
-                            contents.append(
-                                Content.from_text(
-                                    text,
-                                    features=[Feature.metadata({"page_num": page_num})],
-                                )
-                            )
-
-            if "image" in params.output_types:
-                doc = fitz.open(inputtmpfile.name)
-                for page_num in range(len(doc)):
-                    page = doc.load_page(page_num)
-                    for img_index, img in enumerate(page.get_images(full=True)):
-                        xref = img[0]
-                        base_image = doc.extract_image(xref)
-                        image_bytes = base_image["image"]
-                        feature = Feature.metadata(
-                            {"page": page_num, "img_num": img_index}
-                        )
-                        contents.append(
-                            Content(
-                                content_type="image/png",
-                                data=image_bytes,
-                                features=[feature],
-                            )
-                        )
-
-            if "table" in params.output_types:
-                if os.environ.get("EXTRACTOR_PATH"):
-                    return []
-                tables = get_tables(content.data)
-                for page_index, content in tables.items():
-                    feature = Feature.metadata({"page": page_index})
+        pdf_parser = PDFParser(data=content.data)
+        pages: List[Page] = pdf_parser.parse()
+        for page in pages:
+            text = ""
+            for fragment in page.fragments:
+                if fragment.fragment_type == PageFragmentType.TEXT:
+                    text += fragment.text
+                if fragment.fragment_type == PageFragmentType.TABLE:
                     contents.append(
-                        Content(
-                            content_type="application/json",
-                            data=json.dumps(content),
-                            features=[feature],
-                        )
+                        Content(content_type="text/html", data=fragment.table.data)
                     )
-
+                if fragment.fragment_type == PageFragmentType.FIGURE:
+                    contents.append(
+                        Content(content_type="image/png", data=fragment.image.data)
+                    )
+            if text != "":
+                contents.append(Content(content_type="text/plain", data=text))
         return contents
 
     def sample_input(self) -> Content:
@@ -90,9 +47,9 @@ class PDFExtractor(Extractor):
 
 
 if __name__ == "__main__":
-    f = open("2310.16944.pdf", "rb")
+    f = open("1706.03762v7.pdf", "rb")
     pdf_data = Content(content_type="application/pdf", data=f.read())
     extractor = PDFExtractor()
-    params = PDFExtractorConfig(output_types=["text", "table"])
+    params = PDFExtractorConfig()
     results = extractor.extract(pdf_data, params)
     print(results)
